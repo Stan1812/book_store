ce424df897ba361259290f1c1f76abb7c8006b1b
stan1812 <2314980309@qq.com>
2018-11-21 14:15:18 +0800

    init

---
+++ src/main/java/com/expr/bookstore/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.expr.bookstore;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
