326c03ab9b589173c6c3afad583c737fe44871d9
stan1812 <2314980309@qq.com>
2018-11-30 15:39:43 +0800

    fix api and some params

--- src/main/java/com/expr/bookstore/controllers/BookController.java
+++ src/main/java/com/expr/bookstore/controllers/BookController.java
@@ -4,10 +4,7 @@ import com.expr.bookstore.entity.Book;
 import com.expr.bookstore.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,32 +15,39 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @PostMapping(path = "/getAllBooks")
+    @GetMapping(path = "/getAllBooks")
     public @ResponseBody
     List<Book> getAllBooks() {
         return bookService.queryAllBooks();
     }
-
     @PostMapping(path = "/add")
-    public @ResponseBody Book addNewBook(@RequestParam String name, @RequestParam String author,
-                                         @RequestParam Double price, @RequestParam String image,
-                                         @RequestParam String description, @RequestParam Long categoryId) {
-        return bookService.addNewBook(name, author, price, image, description, categoryId);
+    public @ResponseBody
+    Book addNewBook(@RequestParam String name,
+                    @RequestParam String author,
+                    @RequestParam Double price,
+                    @RequestParam Double score,
+                    @RequestParam String press,
+                    @RequestParam String image,
+                    @RequestParam String description,
+                    @RequestParam Integer commentNum,
+                    @RequestParam Long categoryId) {
+        return bookService.addNewBook(name, author, press, price, score, image, commentNum, description, categoryId);
     }
 
     @PostMapping(path = "/getAllBooksByCategory")
-    public @ResponseBody List<Book> getAllBooksByCategory(@RequestParam Long categoryId) {
+    public @ResponseBody
+    List<Book> getAllBooksByCategory(@RequestParam Long categoryId) {
         return bookService.queryBooksByCategoryId(categoryId);
     }
-
-    @PostMapping(path = "/getBookById")
-    public @ResponseBody Book getBookById(@RequestParam Long id) {
+    @GetMapping(path = "/getBookById")
+    public @ResponseBody
+    Book getBookById(@RequestParam Long id) {
         return bookService.queryBooksById(id);
     }
 
     @PostMapping(path = "/getBookByName")
-    public @ResponseBody Book getBookByName(@RequestParam String name) {
+    public @ResponseBody
+    Book getBookByName(@RequestParam String name) {
         return bookService.queryBookByName(name);
     }
-
 }
--- src/main/java/com/expr/bookstore/services/BookServiceImpl.java
+++ src/main/java/com/expr/bookstore/services/BookServiceImpl.java
@@ -1,7 +1,7 @@
 package com.expr.bookstore.services;
 
-import com.expr.bookstore.entity.Book;
 import com.expr.bookstore.dao.BookRepository;
+import com.expr.bookstore.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,22 +18,26 @@ public class BookServiceImpl implements BookService {
 
     /**
      * 添加书籍到数据库
-     * @param name 书名
-     * @param author 作者
-     * @param price 价格
-     * @param image 图片路径
+     *
+     * @param name        书名
+     * @param author      作者
+     * @param press       出版社
+     * @param score       评分
+     * @param price       价格
+     * @param image       图片路径
      * @param description 书籍简介
-     * @param categoryId 书籍类别
+     * @param  commentNum  评价数
+     * @param categoryId  书籍类别
      * @return book
      */
     @Override
-    public Book addNewBook(String name, String author, Double price, String image, String description, Long categoryId) {
-        Book book = new Book(name, author, price, image, description, categoryId);
+    public Book addNewBook(String name, String author, String press, Double price, Double score, String image, Integer commentNum, String description, Long categoryId) {
+        Book book = new Book( name, author, press, price, score, image, commentNum, description, categoryId);
         return bookRepo.save(book);
     }
-
     /**
      * 查询所有书籍
+     *
      * @return books 所有书籍的列表
      */
     @Override
@@ -43,6 +47,7 @@ public class BookServiceImpl implements BookService {
 
     /**
      * 通过书籍类别查询书籍
+     *
      * @param categoryId 书籍类别
      * @return books 对应类别的所有书籍
      */
@@ -53,6 +58,7 @@ public class BookServiceImpl implements BookService {
 
     /**
      * 通过id查找书籍
+     *
      * @param id 书籍id
      * @return book 查找到的书籍
      */
@@ -63,6 +69,7 @@ public class BookServiceImpl implements BookService {
 
     /**
      * 通过书名查询书籍
+     *
      * @param name 书名
      * @return book 对应书名的书籍
      */
